264714ec8a41860abd8740e8a05a48ea076b6c14
BANKAAIII <ameyaswe.com>
2025-05-04 15:43:01 +0530

    Inatilization of project

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+   <div className="flex flex-center justify-center items-center h-screen text-6xl shadow-xl text-amber-300 font-bold font-serif " >
+   Ameya
+   </div>
+  );
+}
